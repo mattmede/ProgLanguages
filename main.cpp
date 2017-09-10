6dcb93af796664fc101ef00084ddc41ca1bd0849
Matt <mam361@zips.uakron.edu>
2017-09-10 17:35:28 -0400

    Added skeleton of main implementing nHeight and bHeight

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include "domains.hpp"
+
+int main() {
+    
+    //Height of Numeric AST
+    auto* nheight = new int_lit {
+        //Figure out what kind of tree it is
+        //Figure out height based on that
+    }
+
+    //Height of Boolean AST
+    auto* bheight = new arith_expr {
+        //Figure out what kind of tree it is
+        //Figure out height based on that
+    }
+}
